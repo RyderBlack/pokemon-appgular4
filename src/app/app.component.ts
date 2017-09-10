d0043856045257662bd86401decf7411b1989ca6
RyderBlack <ryhad.boughanmi@gmail.com>
2017-09-10 18:05:17 +0200

    changing angular version 2 to 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,6 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'pokemon-app',
-	template: `<h1>Hello, Angular 2 !</h1>`,
+	template: `<h1>Hello, Angular 4 ;) !</h1>`,
 })
 export class AppComponent { }
