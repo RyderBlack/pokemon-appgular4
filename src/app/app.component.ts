511d44576d7c370d5de14eb3108f621ab058fdaf
RyderBlack <ryhad.boughanmi@gmail.com>
2017-09-12 19:36:43 +0200

    update files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,24 @@ export class AppComponent {
 
 */
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
 
 @Component({
-	selector: 'pokemon-app',
-	template: `<h1>Hello, Angular 4 ;) !</h1>`,
+  selector: 'pokemon-app',
+  template: '<h1>Pokémons</h1>'
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  pokemons: Pokemon[] = null;
+
+  ngOnInit() {
+    this.pokemons = POKEMONS;
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log('Vous avez selectionné ' + pokemon.name);
+  }
+  
+}
